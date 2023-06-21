4d20f8955723c3b1d38c3db568634952ce8167d9
kage4cd <127705392+kage4cd@users.noreply.github.com>
2023-06-21 17:30:15 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,16 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	// Loggerクラスのインスタンスを生成する
+        final Logger logger = Logger.getLogger("SampleLogging");
+	
 	@RequestMapping("/")
 	String sayHello() {
+		// ログを出力する
+		logger.log(Level.FINEST, "ログ出力テスト：finest");
+		logger.log(Level.FINER, "ログ出力テスト：finer");
+		logger.log(Level.FINE, "ログ出力テスト：fine");
+		logger.log(Level.CONFIG, "ログ出力テスト：config");
+		logger.log(Level.INFO, "ログ出力テスト：info");
+		logger.log(Level.WARNING, "ログ出力テスト：warning");
+		logger.log(Level.SEVERE, "ログ出力テスト：severe");
 		return "Hello World!";
 	}
 }
