5dd3bbe86b44e992831f90ce24c88a97e01c3c61
kage4cd <127705392+kage4cd@users.noreply.github.com>
2023-06-21 17:52:10 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	
 
 	public static void main(String[] args) {
@@ -21,12 +18,6 @@ public class DemoApplication {
 	
 	@RequestMapping("/")
 	String sayHello() {
-		// ログを出力する
-		logger.trace("ログ出力テスト：trace");
-		logger.debug("ログ出力テスト：debug");
-		logger.info("ログ出力テスト：info");
-		logger.warn("ログ出力テスト：warn");
-		logger.error("ログ出力テスト：error");
 		return "Hello World!";
 	}
 }
