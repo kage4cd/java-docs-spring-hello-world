9b9c5b797d3bc23b4ecba997e0a2b6ce65a9a884
kage4cd <127705392+kage4cd@users.noreply.github.com>
2023-07-04 14:39:49 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,41 +16,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	
 	@RequestMapping("/")
 	String sayHello() {
-		// ロガーを取得（ロギングの対象のパッケージ名やクラス名を引数とする）
-    		Logger logger = Logger.getLogger("LoggingSample");
-	        try{
-	            // ログレベルを設定（INFOに設定した場合これより低いレベルのメッセージは破棄される）
-	            logger.setLevel(Level.INFO);
-	
-	          // ハンドラーを作成してロガーに追加
-	            Handler handler = new FileHandler("sample.log");
-	            logger.addHandler(handler);
-	
-	            // フォーマッターを作成してハンドラーに登録
-	            Formatter formatter = new SimpleFormatter();
-	            handler.setFormatter(formatter);
-	
-	            // それぞれのログレベルのメッセージを出力（指定されたログレベル以上のものだけが出力される）
-	            logger.finest("最も詳細な（FINEST）メッセージ");
-	            logger.finer("詳細な（FINER）メッセージ");
-	            logger.fine("通常（FINE）メッセージ");
-	            logger.config("構成（CONFIG）メッセージ");
-	            logger.info("情報（INFO）メッセージ");
-	            logger.warning("警告（WARNING）メッセージ");
-	            logger.severe("重大（SEVERE）メッセージ");
-	
-	            // log()を使用して指定のログレベルメッセージを出力
-	            logger.log(Level.INFO, "情報（INFO）メッセージ");
-		    return "Hello World!";
-	            throw new Exception();
-	        } catch (Exception e) {
-	            // 引数に渡された例外クラスのスタックトレースを出力
-	            logger.log(Level.WARNING, "エラーが発生しました", e);
-	        }
-	    }
-		
+		return "Hello World!";
+	}
 }
